1b3a26268ac360da40f840301060a299644a8efd
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-08-01 22:06:35 +0530

    Create Lab - 1.sh

---
+++ CN Lab/Lab - 1/Lab - 1.sh
@@ -0,0 +1,31 @@
+# Terminal Commands to make a virtual environment:
+python3 -m venv gns3env
+
+# Activate the virtual environment:
+source gns3env/bin/activate
+
+# Install required libraries:
+pip install pyqt5
+pip install gns3-server
+pip install gns3-gui
+
+# Execute this under virtual environment:
+gns3
+
+# In console, assign an IP to computer by running the following commands:
+ip 192.168.1.1 255.255.255.0
+
+# For Virtual PC 2 onwards:
+ip 192.168.1.2/24 # Change last decimal for consecutive PCs.
+
+# Ping a PC:
+ping 192.168.1.2 # Assuming you are PC1 right now.
+
+# To define number of packets to ping a PC:
+ping 192.168.1.2 -c 5 # Sends 5 packets to PC2.
+
+# Saving terminal commands:
+save # Execute this while being inside terminal of any PC.
+
+# View information about assigned commands to a PC:
+show ip
