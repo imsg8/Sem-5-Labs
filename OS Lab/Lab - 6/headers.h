e21a683909816662641d73a86e32eb1e1882975c
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-08-26 01:09:50 +0530

    Update headers.h

--- OS Lab/Lab - 6/headers.h
+++ OS Lab/Lab - 6/headers.h
@@ -1,10 +1,12 @@
-#include <sys/wait.h>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+#define FIFO_NAME "my_fifo"
+#define BUFFER_SIZE 6
