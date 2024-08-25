43464e938d7facd3f4726e2cf983a30b52b76786
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-08-26 00:57:46 +0530

    Create headers.h

---
+++ OS Lab/Lab - 6/headers.h
@@ -0,0 +1,10 @@
+#include <sys/wait.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <limits.h>
