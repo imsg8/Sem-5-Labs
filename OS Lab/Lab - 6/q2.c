3a3d2dcfd83b1d398428fcc9e46bc55cbed76a07
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-09-09 00:27:08 +0530

    Update q2.c

--- OS Lab/Lab - 6/q2.c
+++ OS Lab/Lab - 6/q2.c
@@ -2,24 +2,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#define FIFO_NAME "my_fifo"
-#define BUFFER_SIZE 6
+
+#define BUFFER_SIZE 1024
 
 int main(int argc, char **argv){
     int n, fd[2];
-    char buf[1025], data = "hello... this is sample data";
-    pipe(fd);
-    write(fd[1], data, strlen(data));
-    if ((n = read(fd[0], buf, 1024)) >= 0) {
-        buf[n] = 0;
-        printf("read %d bytes from the pipe: \"%s\"\n", n, buf);
-    } else {
-        perror("read");
+    char buf[BUFFER_SIZE + 1];
+    const char *data = "hello... this is sample data";
+
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
     }
-    exit(0);
+
+    if (write(fd[1], data, strlen(data)) == -1) {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(fd[1]) == -1) {
+        perror("close write end");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((n = read(fd[0], buf, BUFFER_SIZE)) == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+
+    buf[n] = '\0';
+    printf("Read %d bytes from the pipe: \"%s\"\n", n, buf);
+
+    if (close(fd[0]) == -1) {
+        perror("close read end");
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
 }
