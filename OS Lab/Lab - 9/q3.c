6fafa4f30e4b1dceacf7fae9912bd82ccec76ad3
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-09-30 02:17:04 +0530

    Update q3.c

--- OS Lab/Lab - 9/q3.c
+++ OS Lab/Lab - 9/q3.c
@@ -1,32 +1,29 @@
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int n, pr[100];
-
-void* child_thread(void * param) {
-    int x = (int)param;
-    for(int i = 2; i < x; i++) {
-        if(x % i == 0)
-            return 0;
-    }
+void* child_thread(void* param) {
+    int x = (long)param;
+    for (int i = 2; i < x; i++)
+        if (x % i == 0) return 0;
     pr[n++] = x;
     return 0;
 }
+
 int main() {
     pthread_t thread[100];
-    int return_value[100];
-    n = 0;
     int l, u;
-    printf("Enter lower limit ");
+    printf("Enter lower limit: ");
     scanf("%d", &l);
-    printf("Enter upper limit ");
+    printf("Enter upper limit: ");
     scanf("%d", &u);
+
     for (int i = l; i < u; i++) {
-        pthread_create(&thread[i], 0, &child_thread, (void*)i);
-        pthread_join(thread[i], (void**)&return_value[i]);
+        pthread_create(&thread[i - l], 0, child_thread, (void*)(long)i);
+        pthread_join(thread[i - l], 0);
     }
-    printf("Prime Numbers in the range: \n");
+
+    printf("Prime Numbers in the range:\n");
     for (int i = 0; i < n; i++)
         printf("%d ", pr[i]);
 }
