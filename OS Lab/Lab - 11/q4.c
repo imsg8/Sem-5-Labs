38df096d946021f3fd9bc61cc82844caee423a01
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-10-21 00:30:28 +0530

    Update q4.c

--- OS Lab/Lab - 11/q4.c
+++ OS Lab/Lab - 11/q4.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define dsksz 100
+#define sparesz 10
+#define filesz 5
 
-#define DISK_SIZE 100
-#define SPARE_SIZE 10
-#define FILE_SIZE 5
+int disk[dsksz];
+int spare[sparesz];
+int bad_block_table[dsksz];
 
-int disk[DISK_SIZE];
-int spare[SPARE_SIZE];
-int bad_block_table[DISK_SIZE];
-
-void initialize_disk() {
-    for (int i = 0; i < DISK_SIZE; i++) disk[i] = 0;
-    for (int i = 0; i < DISK_SIZE; i++) bad_block_table[i] = -1;
+void initDisk() {
+    for (int i = 0; i < dsksz; i++) disk[i] = 0;
+    for (int i = 0; i < dsksz; i++) bad_block_table[i] = -1;
 }
 
-void allocate_file(int start) {
-    for (int i = start; i < start + FILE_SIZE; i++) {
+void alloc_file(int start) {
+    for (int i = start; i < start + filesz; i++) {
         if (disk[i] == -1) {
-            for (int j = 0; j < SPARE_SIZE; j++) {
+            for (int j = 0; j < sparesz; j++) {
                 if (spare[j] == 0) {
                     spare[j] = 1;
-                    bad_block_table[i] = DISK_SIZE + j;
-                    printf("Bad block at %d, replaced by spare block %d\n", i, DISK_SIZE + j);
+                    bad_block_table[i] = dsksz + j;
+                    printf("Bad block at %d, replaced by spare block %d\n", i, dsksz + j);
                     break;
                 }
             }
@@ -31,7 +30,7 @@ void allocate_file(int start) {
     }
 }
 
-void access_block(int block) {
+void acc_blk(int block) {
     if (bad_block_table[block] != -1)
         printf("Accessing spare block %d (replaces bad block %d)\n", bad_block_table[block], block);
     else
@@ -39,18 +38,32 @@ void access_block(int block) {
 }
 
 int main() {
-    initialize_disk();
+    initDisk();
 
     disk[2] = -1;  
     disk[4] = -1;
 
     printf("Allocating file starting at block 0:\n");
-    allocate_file(0);
+    alloc_file(0);
 
     printf("\nAccessing all blocks of the file:\n");
-    for (int i = 0; i < FILE_SIZE; i++) {
-        access_block(i);
+    for (int i = 0; i < filesz; i++) {
+        acc_blk(i);
     }
 
     return 0;
 }
+
+// SAMPLE INPUT - OUTPUT
+
+// Allocating file starting at block 0:
+// Bad block at 2, replaced by spare block 100
+// Bad block at 4, replaced by spare block 101
+
+// Accessing all blocks of the file:
+// Accessing block 0
+// Accessing block 1
+// Accessing spare block 100 (replaces bad block 2)
+// Accessing block 3
+// Accessing spare block 101 (replaces bad block 4)
+
