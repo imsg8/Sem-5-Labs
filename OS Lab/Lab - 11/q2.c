9440641221855af0439ee77fabac21e8be48afdd
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-10-21 00:33:51 +0530

    Update q2.c

--- OS Lab/Lab - 11/q2.c
+++ OS Lab/Lab - 11/q2.c
@@ -12,7 +12,7 @@ int avl[maxres];
 int maxi[maxt][maxres];
 int allocation[maxt][maxres];
 int need[maxt][maxres];
-int num_threads, num_resources;
+int numth, numres;
 
 pthread_mutex_t lock;
 
@@ -20,13 +20,13 @@ bool is_safe(int thread_id) {
     int work[maxres];
     bool finish[maxt] = {false};
     
-    for (int i = 0; i < num_resources; i++)
+    for (int i = 0; i < numres; i++)
         work[i] = avl[i];
     
-    for (int i = 0; i < num_threads; i++) {
+    for (int i = 0; i < numth; i++) {
         if (!finish[i]) {
             bool can_allocate = true;
-            for (int j = 0; j < num_resources; j++) {
+            for (int j = 0; j < numres; j++) {
                 if (need[i][j] > work[j]) {
                     can_allocate = false;
                     break;
@@ -34,14 +34,14 @@ bool is_safe(int thread_id) {
             }
             if (can_allocate) {
                 finish[i] = true;
-                for (int j = 0; j < num_resources; j++)
+                for (int j = 0; j < numres; j++)
                     work[j] += allocation[i][j];
                 i = -1;
             }
         }
     }
     
-    for (int i = 0; i < num_threads; i++)
+    for (int i = 0; i < numth; i++)
         if (!finish[i])
             return false;
     return true;
@@ -52,13 +52,13 @@ void* thread_function(void* arg) {
     
     while (1) {
         int request[maxres];
-        for (int i = 0; i < num_resources; i++)
+        for (int i = 0; i < numres; i++)
             request[i] = rand() % (maxi[thread_id][i] - allocation[thread_id][i] + 1);
         
         pthread_mutex_lock(&lock);
         
         bool can_allocate = true;
-        for (int i = 0; i < num_resources; i++) {
+        for (int i = 0; i < numres; i++) {
             if (request[i] > avl[i] || request[i] > need[thread_id][i]) {
                 can_allocate = false;
                 break;
@@ -66,7 +66,7 @@ void* thread_function(void* arg) {
         }
         
         if (can_allocate) {
-            for (int i = 0; i < num_resources; i++) {
+            for (int i = 0; i < numres; i++) {
                 avl[i] -= request[i];
                 allocation[thread_id][i] += request[i];
                 need[thread_id][i] -= request[i];
@@ -75,7 +75,7 @@ void* thread_function(void* arg) {
             if (is_safe(thread_id)) {
                 printf("Thread %d: Request granted\n", thread_id);
             } else {
-                for (int i = 0; i < num_resources; i++) {
+                for (int i = 0; i < numres; i++) {
                     avl[i] += request[i];
                     allocation[thread_id][i] -= request[i];
                     need[thread_id][i] += request[i];
@@ -91,7 +91,7 @@ void* thread_function(void* arg) {
         usleep(rand() % 1000000);
         
         pthread_mutex_lock(&lock);
-        for (int i = 0; i < num_resources; i++) {
+        for (int i = 0; i < numres; i++) {
             int release = rand() % (allocation[thread_id][i] + 1);
             allocation[thread_id][i] -= release;
             avl[i] += release;
@@ -110,21 +110,21 @@ int main() {
     srand(time(NULL));
     
     printf("Enter the number of threads: ");
-    scanf("%d", &num_threads);
+    scanf("%d", &numth);
     printf("Enter the number of resources: ");
-    scanf("%d", &num_resources);
+    scanf("%d", &numres);
     
     printf("Enter the avl resources:\n");
-    for (int i = 0; i < num_resources; i++)
+    for (int i = 0; i < numres; i++)
         scanf("%d", &avl[i]);
     
     printf("Enter the maxi resources for each thread:\n");
-    for (int i = 0; i < num_threads; i++)
-        for (int j = 0; j < num_resources; j++)
+    for (int i = 0; i < numth; i++)
+        for (int j = 0; j < numres; j++)
             scanf("%d", &maxi[i][j]);
     
-    for (int i = 0; i < num_threads; i++)
-        for (int j = 0; j < num_resources; j++) {
+    for (int i = 0; i < numth; i++)
+        for (int j = 0; j < numres; j++) {
             allocation[i][j] = 0;
             need[i][j] = maxi[i][j];
         }
@@ -134,12 +134,12 @@ int main() {
     pthread_t threads[maxt];
     int thread_ids[maxt];
     
-    for (int i = 0; i < num_threads; i++) {
+    for (int i = 0; i < numth; i++) {
         thread_ids[i] = i;
         pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]);
     }
     
-    for (int i = 0; i < num_threads; i++)
+    for (int i = 0; i < numth; i++)
         pthread_join(threads[i], NULL);
     
     pthread_mutex_destroy(&lock);
