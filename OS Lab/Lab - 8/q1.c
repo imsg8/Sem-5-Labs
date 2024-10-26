f883d7a787c14bedb926981fe61e2b41fdf05cd0
Shivang Gulati <136248846+imsg8@users.noreply.github.com>
2024-10-26 20:41:25 +0530

    Update q1.c

--- OS Lab/Lab - 8/q1.c
+++ OS Lab/Lab - 8/q1.c
@@ -1,62 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h>
 
-int buf[10],f,r;
-sem_t mutex,full,empty,atmost10;
-int produced,consumed;
+#define sz 10
 
-void* produce(void* arg)
-{
-    while(1)
-    {
-        sem_wait(&atmost10);
-        sem_wait(&empty);
+int arr[sz];
+int produced = 0, consumed = 0;
+sem_t mutex, full, empty, atmost10;
+
+void* pro(void* param) {
+    while (1) {
+        while (produced - consumed >= 10) {
+            usleep(100000);
+        }
+        
+        sem_wait(&empty); 
         sem_wait(&mutex);
-        printf("Produced Item : %d\n",produced);
-        buf[(++r)%10]=produced;
+        
+        int pos = produced % sz;
+        arr[pos] = produced;
+        printf("Produced Item: %d (Difference: %d)\n", produced, produced - consumed);
         produced++;
-        sleep(1);
+        
         sem_post(&mutex);
         sem_post(&full);
-        if(produced-consumed<=10)
-        {
-            sem_post(&atmost10);
-        }
-    }
-}
-
-void* consume(void* arg)
-{
-    int item;
-    while(1)
-    {
-        sem_wait(&full);
-        sem_wait(&mutex);
-        item=buf[(++f)%10];
-        consumed++;
-        printf("Consumed Item : %d\n",item);
-        sem_post(&mutex);
-        sem_post(&empty);
-        if(produced-consumed<=10)
-        {
-            sem_post(&atmost10);
-        }
         sleep(1);
     }
 }
 
-void main()
-{
+void* con(void* arg) {
+    while (1) {
+        sem_wait(&full);
+        sem_wait(&mutex);
+        
+        int pos = consumed % sz;
+        int item = arr[pos];
+        printf("Consumed Item: %d (Difference: %d)\n", item, produced - (consumed + 1));
+        consumed++;
+        
+        sem_post(&mutex);
+        sem_post(&empty);
+        
+        sleep(5);
+    }
+}
+
+int main() {
     pthread_t th[2];
-    sem_init(&mutex,0,1);
-    sem_init(&full,0,0);
-    sem_init(&empty,0,10);
-    sem_init(&atmost10,0,1);
-    pthread_create(&th[0],NULL,produce,NULL);
-    pthread_create(&th[1],NULL,consume,NULL);
-    pthread_join(th[0],NULL);
-    pthread_join(th[1],NULL);
+    
+    sem_init(&mutex, 0, 1);
+    sem_init(&full, 0, 0);
+    sem_init(&empty, 0, sz);
+    sem_init(&atmost10, 0, 1);
+
+    pthread_create(&th[0], NULL, pro, NULL);
+    pthread_create(&th[1], NULL, con, NULL);
+    
+    pthread_join(th[0], NULL);
+    pthread_join(th[1], NULL);
+    
+    sem_destroy(&mutex);
+    sem_destroy(&full);
+    sem_destroy(&empty);
+    sem_destroy(&atmost10);
+    
+    return 0;
 }
